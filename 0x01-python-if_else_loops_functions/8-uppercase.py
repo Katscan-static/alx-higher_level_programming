02508876fe93d00177585ab44728bc13f7374e82
Katekani Madingana <madinganakn@gmail.com>
2023-05-09 17:13:48 -0700

    convert to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) > 96 and ord(i) < 123:
+            print("{:c}".format(ord(i) - 32), end="")
+        else:
+            print("{:c}".format(ord(i)), end="")
+    print()
+
