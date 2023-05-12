ff2e494def0476aacf9fc4030e1bdb53434e20f9
Katekani Madingana <madinganakn@gmail.com>
2023-05-11 18:18:42 -0700

    get args task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,6 @@ if __name__ == "__main__":
     import sys
     len_av = len(sys.argv)
     if len_av > 1:
-        
         if len_av == 2:
             print("1 argument:")
             print("{}: {}".format(len_av - 1, sys.argv[1]))
@@ -13,6 +12,3 @@ if __name__ == "__main__":
                 print("{}: {}".format(i, sys.argv[i]))
     else:
         print("0 arguments.")
-
-
-
