a3c730a005e9349c2eb6027f405159bdaab34882
Katekani Madingana <madinganakn@gmail.com>
2023-05-11 19:06:20 -0700

    hidden task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import importlib.util
-dr = "/root/alx-higher_level_programming/0x02-python-import_modules/"
-spec = importlib.util.spec_from_file_location("hidden_4", dr + "hidden_4.pyc")
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
-names = [name for name in dir(module) if not name.startswith("__")]
-for name in names:
-    print(name)
+if __name__ == "__main__":
+    import importlib.util
+    hd = "./hidden_4.pyc"
+    spec = importlib.util.spec_from_file_location("hidden_4", hd)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    names = [name for name in dir(module) if not name.startswith("__")]
+    for name in names:
+        print(name)
