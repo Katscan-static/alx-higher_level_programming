7df66caec288c4738aa75658b4ecd62cc8b07063
Katekani Madingana <madinganakn@gmail.com>
2023-05-13 18:00:05 -0700

    using import library only it is simpler

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,7 @@
 if __name__ == "__main__":
     import importlib.util
     hd = "./hidden_4.pyc"
-    spec = importlib.util.spec_from_file_location("hidden_4", hd)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
+    module = importlib.import_module("hidden_4", hd)
     names = [name for name in dir(module) if not name.startswith("__")]
     for name in names:
         print(name)
