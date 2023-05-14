128745b9e67cfea53d6042eb8f94598490b8445c
Katekani Madingana <madinganakn@gmail.com>
2023-05-13 18:09:59 -0700

    importing library wothout importlib

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import importlib.util
-    hd = "./hidden_4.pyc"
-    module = importlib.import_module("hidden_4", hd)
-    names = [name for name in dir(module) if not name.startswith("__")]
+    import hidden_4
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
     for name in names:
         print(name)
