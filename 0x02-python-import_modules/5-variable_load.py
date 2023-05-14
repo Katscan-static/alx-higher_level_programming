74fdda35f9f955d01dc0ff76b7c52164c41f6bc3
Katekani Madingana <madinganakn@gmail.com>
2023-05-13 17:53:18 -0700

    variable load using importlib only

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import importlib.util as iu
+    import importlib
     vr = "./variable_load_5.py"
-    spec = iu.spec_from_file_location("variable_load_5", vr)
-    module = iu.module_from_spec(spec)
-    spec.loader.exec_module(module)
+    module = importlib.import_module("variable_load_5", vr)
     a = module.a
     print(a)
