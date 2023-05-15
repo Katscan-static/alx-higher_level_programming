50fa6714799cc8d026376a25c470ea2d9a4a2691
Katekani Madingana <madinganakn@gmail.com>
2023-05-15 09:55:37 -0700

    replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    new_list = []
     if idx < 0 or idx >= len(my_list):
         return None
     else:
-        new_list = my_list.copy()
-        new_list[idx] = element
-    return new_list
+        my_list[idx] = element
+    return my_list
