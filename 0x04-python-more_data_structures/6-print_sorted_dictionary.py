870576cb878796173870237a5d3f4a18f6c0938e
Katekani Madingana <madinganakn@gmail.com>
2023-05-17 12:04:51 -0700

    print sorted dictionary made executable

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/pythom3
 def print_sorted_dictionary(a_dictionary):
-    sorted_dict = {k: a_dictionary[k] for k in sorted(a_dictionary.keys())}
-    print(sorted_dict)
+    for k in sorted(a_dictionary.keys()):
+        print("{}: {}".format(k, a_dictionary[k]))
