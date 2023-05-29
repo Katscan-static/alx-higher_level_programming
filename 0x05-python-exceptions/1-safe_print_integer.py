a4b4c052d0381bf4b4d66221d962126eff65760a
Katekani Madingana <madinganakn@gmail.com>
2023-05-29 12:13:14 -0700

    safe print integer 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, str):
-            if value.isdigit():
-                value = int(value)
+        value = int(value)
         print("{:d}".format(value))
         return True
     except ValueError:
-        False
+        return False
