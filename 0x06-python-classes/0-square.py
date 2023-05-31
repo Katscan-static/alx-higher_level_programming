2857f0a2ad5ee3f3746b34e6fdba5005d6d1220e
Katekani Madingana <madinganakn@gmail.com>
2023-05-31 14:50:57 -0700

    0 empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 class Square:
     """
-    empyty class
+    
+    this class represents a square
+
+    Attributes:
+        there are no attributes in the class
+
+    Methods:
+        there are no methods in the class
     """
     pass
