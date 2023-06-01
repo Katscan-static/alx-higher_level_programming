d76631a93b6c9186b02278957547db39d65eac52
Katekani Madingana <madinganakn@gmail.com>
2023-06-01 02:27:18 -0700

    print square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -59,6 +59,9 @@ class Square:
             print square using #
         """
 
+        if not self.__size:
+            print()
+
         for i in range(0, self.__size):
             for j in range(0, self.__size):
                 print("#", end="")
