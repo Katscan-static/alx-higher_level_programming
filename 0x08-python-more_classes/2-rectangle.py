18080f12623d5cb39d68df9f97566de2ec833b0c
Katekani Madingana <madinganakn@gmail.com>
2023-06-05 12:11:00 -0700

    added an if statement which checks if height or width is 0 and returns 0 if either is found to be 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -94,4 +94,6 @@ class Rectangle:
             Returns:
                 int: 2 * (height + width)
         """
+        if self.height == 0 or self.width == 0:
+            return 0
         return 2 * (self.width + self.height)
