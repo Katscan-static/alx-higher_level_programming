fabac3120dda0380507711fa466eb19b4278688a
Katekani Madingana <madinganakn@gmail.com>
2023-06-13 10:31:20 -0700

    made it pycodestylr compliant

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,7 +6,11 @@
 
 def write_file(filename="", text=""):
     """
+        a function that writes to a file
 
+        Args:
+            filename (str): filename
+            text (str): text
     """
 
     with open(filename, 'w', encoding="utf-8") as f:
