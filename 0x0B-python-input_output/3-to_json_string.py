82321047d1853ffc7e938a65514e1c2930b25253
Katekani Madingana <madinganakn@gmail.com>
2023-06-13 11:25:18 -0700

    returns a string representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+"""
+ A function that returns a json representation
+"""
+
+
+def to_json_string(my_obj):
+    """
+        A function that returns a json representation
+
+        Args:
+            my_obj (class): An object
+    """
+
+    return json.dumps(my_obj)
