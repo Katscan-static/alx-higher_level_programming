d5722b470d9bedd1bf29f15212020483fe1aef46
Katekani Madingana <madinganakn@gmail.com>
2023-06-20 09:52:03 -0700

    test base class without specified id

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -18,15 +18,15 @@ class Test_base(unittest.TestCase):
 
         base_1 = Base(12)
         self.assertEqual(base_1.id, 12, "Id value should be 12")
-        base_2 = Base(1)
+        base_2 = Base()
         self.assertEqual(base_2.id, 1, "Base should be equal to 1")
-        base_3 = Base(2)
+        base_3 = Base()
         self.assertEqual(base_3.id, 2, "Base should be equal to 2")
-        base_4 = Base(3)
+        base_4 = Base()
         self.assertEqual(base_4.id, 3, "Base should be equal to 3")
         base_5 = Base(254)
         self.assertEqual(base_5.id, 254, "Base should be equal to 254")
-        base_6 = Base(4)
+        base_6 = Base()
         self.assertEqual(base_6.id, 4, "Base should be equal to 4")
 
 
