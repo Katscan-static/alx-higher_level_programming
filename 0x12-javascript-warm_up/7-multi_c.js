04b6efb68f8ab0ec086b68897ee8da7c251394e8
Katekani Madingana <madinganakn@gmail.com>
2023-08-14 05:00:59 -0700

    fixed all the occurances

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 const num = parseInt(process.argv[2]);
 let i = 0;
 
-if (isNaN(num)) {
+if (isNaN(num) && process.argv.length > 2) {
   console.log('Missing number of occurrences');
-} else {
+} else if (!isNaN(num)) {
   while (i !== num) {
     console.log('C is fun');
     i++;
