b74ecc8ab1e3b00797773773253440c17cc1de87
Katekani Madingana <madinganakn@gmail.com>
2023-08-15 11:47:08 -0700

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,10 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w < 1 || h < 1) {
-      return {};
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-
-    this.width = w;
-    this.height = h;
   }
 }
 
