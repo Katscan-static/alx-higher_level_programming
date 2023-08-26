61f6f0b2c64838b6a5ff898e0fd6caef4f3032cd
Katekani Madingana <madinganakn@gmail.com>
2023-08-26 08:17:15 -0700

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,7 +19,10 @@ if __name__ == "__main__":
             )
 
     cursor = db.cursor()
-    query = "SELECT * from cities ORDER BY id ASC"
+    query = ("SELECT cities.id, cities.name, states.name "
+             "FROM cities "
+             "JOIN states ON cities.state_id = states.id "
+             "ORDER BY cities.id ASC")
 
     cursor.execute(query)
 
