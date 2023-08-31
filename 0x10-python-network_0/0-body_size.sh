f7e948e7e6e8be25eaad223e66cf1f1ffce30d82
Katekani Madingana <madinganakn@gmail.com>
2023-08-31 12:29:05 -0700

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# get body size of a request
+#get body size of a request
 url="$1"
-response=$(curl -sI "$url" | grep -i "Content-Length" | cut -d' ' -f2)
-echo "$response"
+curl -sI "$url" | grep -i "Content-Length" | cut -d' ' -f2
