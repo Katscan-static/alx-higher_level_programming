e85e4166915cfdb20d524437167da81bb428cc49
Katekani Madingana <madinganakn@gmail.com>
2023-09-01 14:35:19 -0700

    does not execute when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,7 @@ import urllib.request
 import sys
 
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    if 'X-Request-Id' in response.headers:
-        print(response.headers['X-Request-Id'])
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        if 'X-Request-Id' in response.headers:
+            print(response.headers['X-Request-Id'])
