d6e617242189ff48cd95e40e044f4df55eba5df7
Katekani <madinganakn@gmail.com>
2023-09-02 02:10:44 -0700

    task 8 added try and accept

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,9 +14,10 @@ if __name__ == "__main__":
     response = requests.post(url, data={'q': q})
     response_data = response.json()
 
-    if isinstance(response_data, dict) and response_data:
-        print("[{}] {}".format(response_data['id'], response_data['name']))
-    elif not response_data:
-        print("No result")
-    else:
+    try:
+        if response_data:
+            print("[{}] {}".format(response_data['id'], response_data['name']))
+        elif not response_data:
+            print("No result")
+    except as e:
         print("Not a valid JSON")
