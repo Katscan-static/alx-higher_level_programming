f270d87a4c6d9ad86a8ca7538de4d97e58077528
Katekani <madinganakn@gmail.com>
2023-09-01 22:50:45 -0700

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """ posts email to url """
-import urllib
+import urllib.request
+import urllib.parse
 import sys
 
 
 if __name__ == "__main__":
     data = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(data).encode('utf-8')
-    req = urllib.parse.Request(sys.argv[1], data)
 
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
-        print(html.decode("utf-8"))
-        print(content)
+    with urllib.request.urlopen(sys.argv[1], data) as response:
+        print(response.read().decode("utf-8"))
